b7953e174a29f82083b8828a39dd96cdc0129c8f
Luiz Lopes <luizlopes12@outlook.com>
2021-04-07 10:28:54 -0300

    exercicios

---
+++ ex02/script.js
@@ -0,0 +1,34 @@
+heroi = "Hulk";
+if(heroi == "Hulk"){
+    document.write("O personagem é o Hulk")
+}
+else if(heroi == "Captão America"){
+    document.write("O personagem é o Capitão america");
+}
+else if(heroi == "Homem de Ferro"){
+    document.write("O personagem é o Homem de Ferro");
+}
+else if(heroi == "Pantera Negra"){
+    document.write("O personagem é o Pantera Negra");
+}
+else if(heroi == "Viuva negra"){
+    document.write("O personagem é o Viuva negra");
+}
+else if(heroi == "Thor"){
+    document.write("O personagem é o Thor");
+}
+else if(heroi == "Feiticeira escarlate"){
+    document.write("O personagem é o Feiticeira escarlate");
+}
+else if(heroi == "Vespa"){
+    document.write("O personagem é o Vespa");
+}
+else if(heroi == "Homem Aranha"){
+    document.write("O personagem é o Homem Aranha");
+}
+else if(heroi == "Homem Formiga"){
+    document.write("O personagem é o Homem Formiga");
+}
+else{
+    document.write("Personagem inválido");
+}
